79eba4ce5066eab6430d0a36c012afdbd3112334
kviatek <pkviatek87@wp.pl>
2018-08-13 17:43:49 +0200

    Second update

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,22 +7,26 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    return HttpResponse('<h1>Hello world!</h1>')
+    # return HttpResponse('<h1>Hello world!</h1>')
+    return render(request, 'home.html', {})
 
 
 def contact_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    return HttpResponse('<h1>Contact Page</h1>')
+    # return HttpResponse('<h1>Contact Page</h1>')
+    return render(request, 'contact.html', {})
 
 
 def about_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    return HttpResponse('<h1>About Page</h1>')
+    # return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'about.html', {})
 
 
 def social_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    return HttpResponse('<h1>Social Page</h1>')
+    # return HttpResponse('<h1>Social Page</h1>')
+    return render(request, 'social.html', {})
