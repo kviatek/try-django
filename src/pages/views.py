41e5f040b736137f70194cab20ec597d0e623677
kviatek <pkviatek87@wp.pl>
2018-08-14 12:23:56 +0200

    Try Django, third commit

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,26 +7,28 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    # return HttpResponse('<h1>Hello world!</h1>')
-    return render(request, 'home.html', {})
+    my_context = {'my_text': 'This is a start page', 'my_title': 'Home Page'}
+    return render(request, 'home.html', my_context)
 
 
 def contact_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    # return HttpResponse('<h1>Contact Page</h1>')
-    return render(request, 'contact.html', {})
+    my_context = {'my_text': 'Contact details can be found below', 'my_title': 'Contact details'}
+    return render(request, 'contact.html', my_context)
 
 
 def about_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    # return HttpResponse('<h1>About Page</h1>')
-    return render(request, 'about.html', {})
+    my_context = {'my_text': 'This is about us', 'my_title': 'About us'}
+    return render(request, 'about.html', my_context)
 
 
 def social_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request)
-    # return HttpResponse('<h1>Social Page</h1>')
-    return render(request, 'social.html', {})
+    my_links = ('https://www.facebook.com/pkviatek', 'https://github.com/kviatek')
+    my_context = {'my_text': 'Links to our social media can be found below', 'my_title': 'Social Media',
+                  'links': my_links}
+    return render(request, 'social.html', my_context)
